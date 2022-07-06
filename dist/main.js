66f60552c565ca4a4955f2d16af7947521e13def
krishna <moorthysk108@gmail.com>
2022-07-06 10:10:25 -0700

    webpack calculatot demo

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var r,o,t,u={801:r=>{r.exports={sum:function(r,o){return r+o},multiply:function(r,o){return r*o}}}},n={};function e(r){var o=n[r];if(void 0!==o)return o.exports;var t=n[r]={exports:{}};return u[r](t,t.exports,e),t.exports}r=e(801),e(801),o=r.sum(10,20),t=r.multiply(6,2),console.log("sum"+o),console.log("multiply"+t)})();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+var calculate = require("./calculate.js");
+var tools = require("./calculate.js");
+var sumvalue = calculate.sum(10, 20);
+var mulvalue = calculate.multiply(6, 2);
+
+console.log("sum" + sumvalue);
+console.log("multiply" + mulvalue)
